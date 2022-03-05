de75c5aa2c09aaf21462fbe00281ab241fda106a
ghazaelpo <genarohazael@gmail.com>
2022-03-05 12:59:28 -0600

    new fifos example added

---
+++ FifosExample.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    //mknod() - mkfifo()
+    int fd[2];//0 - lectura 1 - escritura
+    pipe(fd);
+    switch(fork())
+    {
+        case 0: //hijo - recibir
+        close(fd[1]);
+        break;
+        
+        default: //padre
+        close(fd[0]);
+        break;
+    }
+    return 0;
+}
